4d3c7e0dbb12a65b1c3ceb0b1935de2ccbfb914d
Jose-R-OG <jose_ortega2@ucne.edu.do>
2026-01-18 17:03:06 -0400

    Cambios en la logica de privacidad de los pages y cambios en la mensajes de requied

--- ProyectoDeRecuperacion/Models/Productos.cs
+++ ProyectoDeRecuperacion/Models/Productos.cs
@@ -6,8 +6,11 @@ public class Productos
 {
     [Key]
     public int ProductoId { get; set; }
+    [Required(ErrorMessage = "La descripcion es un campo obligatorio")]
     public string Descripcion { get; set; } = string.Empty;
+    [Range(0.1, double.MaxValue, ErrorMessage = "El precio tiene que ser mayor a 0")]
     public double Precio { get; set; }
+    [Range(0, double.MaxValue, ErrorMessage = "El costo tiene que ser mayor a 0")]
     public double Costo { get; set; }
     public int Existencia { get; set; }
 }
